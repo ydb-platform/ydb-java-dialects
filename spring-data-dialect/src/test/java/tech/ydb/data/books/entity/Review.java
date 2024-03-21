8a874135efb45ccccc9d122f3dbbbe4f1565ef44
Madiyar Nurgazin <47685738+SnRise@users.noreply.github.com>
2024-03-21 19:15:44 +0300

    Add integration tests (#90)

---
+++ spring-data-dialect/src/test/java/tech/ydb/data/books/entity/Review.java
@@ -0,0 +1,25 @@
+package tech.ydb.data.books.entity;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+
+/**
+ * @author Madiyar Nurgazin
+ */
+@Data
+@Table("reviews")
+public class Review {
+    @Id
+    private long id;
+    private long readerId;
+    private String text;
+    private long rating;
+    private LocalDateTime created;
+
+    public Review() {
+        this.created = LocalDateTime.now();
+    }
+}
