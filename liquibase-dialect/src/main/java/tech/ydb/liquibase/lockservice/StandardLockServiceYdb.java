8c0f7c9adab31e730c5cddf6ee9f4502cbc0fe9c
Kirill Kurdyukov <kurdyukov-kir@ydb.tech>
2024-03-25 12:36:20 +0300

    feat: [Liquibase] impl StandardLockServiceYdb.java

---
+++ liquibase-dialect/src/main/java/tech/ydb/liquibase/lockservice/StandardLockServiceYdb.java
@@ -0,0 +1,59 @@
+package tech.ydb.liquibase.lockservice;
+
+import java.util.concurrent.ThreadLocalRandom;
+import liquibase.Scope;
+import liquibase.exception.DatabaseException;
+import liquibase.exception.LockException;
+import liquibase.lockservice.StandardLockService;
+
+/**
+ * @author Kirill Kurdyukov
+ */
+public class StandardLockServiceYdb extends StandardLockService {
+
+    private static final int RELEASE_MAX_ATTEMPT = 10;
+
+    @Override
+    public void waitForLock() throws LockException {
+        try {
+            database.getConnection().setAutoCommit(false);
+        } catch (DatabaseException e) {
+            throw new LockException(e);
+        }
+
+        super.waitForLock();
+    }
+
+    @Override
+    public boolean acquireLock() {
+        try {
+            return super.acquireLock();
+        } catch (LockException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void releaseLock() throws LockException {
+        for (int i = 0; i < RELEASE_MAX_ATTEMPT; i++) {
+            try {
+                super.releaseLock();
+
+                Scope.getCurrentScope().getLog(getClass()).info("Release lock!");
+                return;
+            } catch (LockException e) {
+                if (i == RELEASE_MAX_ATTEMPT - 1) {
+                    throw e;
+                }
+
+                Scope.getCurrentScope().getLog(StandardLockServiceYdb.class).info("Retry release lock!");
+
+                try {
+                    Thread.sleep(ThreadLocalRandom.current().nextLong(1000));
+                } catch (InterruptedException exception) {
+                    throw new RuntimeException(exception);
+                }
+            }
+        }
+    }
+}
