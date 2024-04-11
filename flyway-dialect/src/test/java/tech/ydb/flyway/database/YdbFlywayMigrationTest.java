0c679a8b3767c9db47655e9a39bc8acbdbf2cbf6
Kirill Kurdyukov <kurdyukov-kir@ydb.tech>
2024-04-11 16:01:47 +0300

    [Flyway]:
    1) SCAN SELECT -> SELECT FROM flyway schema history table
    2) Tested Info command
    3) Tested Repair command
    4) Tested Validate command

--- flyway-dialect/src/test/java/tech/ydb/flyway/database/YdbFlywayMigrationTest.java
+++ flyway-dialect/src/test/java/tech/ydb/flyway/database/YdbFlywayMigrationTest.java
@@ -36,24 +36,26 @@ public class YdbFlywayMigrationTest extends YdbFlywayBaseTest {
     void evolutionConcurrencySchemaTest() throws ExecutionException, InterruptedException {
         int threadPoolSize = 10;
 
-        ExecutorService threadPool = Executors.newFixedThreadPool(threadPoolSize);
+        try (ExecutorService threadPool = Executors.newFixedThreadPool(threadPoolSize)) {
 
-        for (int migrationStep = 0; migrationStep < EVOLUTION_SCHEMA_MIGRATION_DIRS.length; migrationStep++) {
-            List<Future<?>> taskFutures = new ArrayList<>();
+            for (int migrationStep = 0; migrationStep < EVOLUTION_SCHEMA_MIGRATION_DIRS.length; migrationStep++) {
+                List<Future<?>> taskFutures = new ArrayList<>();
 
-            for (int i = 0; i < threadPoolSize * 2; i++) {
-                int finalMigrationStep = migrationStep;
+                for (int i = 0; i < threadPoolSize * 2; i++) {
+                    int finalMigrationStep = migrationStep;
 
-                taskFutures.add(
-                        threadPool.submit(() -> assertTrue(
-                                createFlyway("classpath:db/" + EVOLUTION_SCHEMA_MIGRATION_DIRS[finalMigrationStep])
-                                        .load().migrate().success
-                        ))
-                );
-            }
+                    taskFutures.add(
+                            threadPool.submit(() -> assertTrue(
+                                    createFlyway("classpath:db/" +
+                                                    EVOLUTION_SCHEMA_MIGRATION_DIRS[finalMigrationStep])
+                                            .load().migrate().success
+                            ))
+                    );
+                }
 
-            for (Future<?> taskFuture : taskFutures) {
-                taskFuture.get();
+                for (Future<?> taskFuture : taskFutures) {
+                    taskFuture.get();
+                }
             }
         }
     }
