140351d2ab0347bd34e0259d93ac30cb5801f136
Kirill Kurdyukov <kurdyukov-kir@ydb.tech>
2024-08-21 18:44:31 +0300

    delete change hibernate

--- hibernate-dialect/src/main/java/tech/ydb/hibernate/dialect/translator/YdbSqlAstTranslator.java
+++ hibernate-dialect/src/main/java/tech/ydb/hibernate/dialect/translator/YdbSqlAstTranslator.java
@@ -1,14 +1,11 @@
 package tech.ydb.hibernate.dialect.translator;
 
-import java.util.List;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
 import org.hibernate.query.sqm.FetchClauseType;
 import org.hibernate.sql.ast.Clause;
 import org.hibernate.sql.ast.spi.AbstractSqlAstTranslator;
 import org.hibernate.sql.ast.tree.Statement;
 import org.hibernate.sql.ast.tree.expression.Expression;
-import org.hibernate.sql.ast.tree.from.FromClause;
-import org.hibernate.sql.ast.tree.from.TableGroupJoin;
 import org.hibernate.sql.exec.spi.JdbcOperation;
 
 /**
@@ -46,15 +43,5 @@ public class YdbSqlAstTranslator<T extends JdbcOperation> extends AbstractSqlAst
                 getClauseStack().pop();
             }
         }
-    }
-
-    @Override
-    protected void renderTableGroupJoin(TableGroupJoin tableGroupJoin, List<TableGroupJoin> tableGroupJoinCollector) {
-        super.renderTableGroupJoin(tableGroupJoin, tableGroupJoinCollector);
-    }
-
-    @Override
-    public void visitFromClause(FromClause fromClause) {
-        super.visitFromClause(fromClause);
     }
 }
