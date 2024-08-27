b2307ccffc95a37377684a423371eba1b35ddb3a
Kirill Kurdyukov <kurdyukov-kir@ydb.tech>
2024-08-27 10:43:53 +0300

    update

---
+++ spring-data-dialect/src/test/resources/changelogs/insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO reviews (id, text, rating, created, book_id, reader)
+VALUES (1, 'Masterpiece!', 10, TIMESTAMP('2024-03-19T15:52:26Z'), 1, 'Ivan Ivanov'),
+       (2, 'Complex work, but I liked it', 9, TIMESTAMP('2024-03-19T16:14:05Z'), 1, 'Sergey Petrov')
