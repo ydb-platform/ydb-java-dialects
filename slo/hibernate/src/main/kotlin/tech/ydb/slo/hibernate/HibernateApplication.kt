9807350b337e4fc260854426555f70cd1695b897
Kirill Kurdyukov <kurdyukov-kir@ydb.tech>
2024-08-30 12:26:38 +0300

    feat: SLO Hibernate

--- slo/hibernate/src/main/kotlin/tech/ydb/slo/hibernate/HibernateApplication.kt
+++ slo/hibernate/src/main/kotlin/tech/ydb/slo/hibernate/HibernateApplication.kt
@@ -20,7 +20,7 @@ class HibernateApplication : CommandLineRunner {
 
 	override fun run(vararg args: String) {
 		val pushGateway = PushGateway.builder()
-			.job("slo-hibernate")
+			.job("workload-hibernate")
 			.address("prometheus-pushgateway:9091")
 			.build()
 
