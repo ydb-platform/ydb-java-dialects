50aba4b75ba20bf0f9162ac3e0f6af2c01fc3d87
Kirill Kurdyukov <kurdyukov-kir@ydb.tech>
2024-11-14 13:30:55 +0300

    feat: added leader info in semaphore data

--- shedlock-ydb/src/main/java/tech/ydb/lock/provider/YdbCoordinationServiceLockProvider.java
+++ shedlock-ydb/src/main/java/tech/ydb/lock/provider/YdbCoordinationServiceLockProvider.java
@@ -1,15 +1,20 @@
 package tech.ydb.lock.provider;
 
+import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.Optional;
+import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import net.javacrumbs.shedlock.core.LockConfiguration;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.core.SimpleLock;
+import net.javacrumbs.shedlock.support.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.ydb.coordination.CoordinationClient;
+import tech.ydb.coordination.CoordinationSession;
 import tech.ydb.coordination.SemaphoreLease;
+import tech.ydb.core.Result;
 import tech.ydb.jdbc.YdbConnection;
 
 /**
@@ -19,21 +24,38 @@ public class YdbCoordinationServiceLockProvider implements LockProvider {
     private static final Logger logger = LoggerFactory.getLogger(YdbCoordinationServiceLockProvider.class);
     private static final String YDB_LOCK_NODE_NAME = "shared-lock-ydb";
     private static final int ATTEMPT_CREATE_NODE = 10;
+    private static final String INSTANCE_INFO =
+            "{Hostname=" + Utils.getHostname() + ", " + "Current PID=" + ProcessHandle.current().pid() + "}";
+    private static final byte[] INSTANCE_INFO_BYTES = INSTANCE_INFO.getBytes(StandardCharsets.UTF_8);
 
     private final YdbConnection ydbConnection;
     private final CoordinationClient coordinationClient;
+
+    private volatile CoordinationSession coordinationSession;
 
     public YdbCoordinationServiceLockProvider(YdbConnection ydbConnection) {
         this.ydbConnection = ydbConnection;
         this.coordinationClient = CoordinationClient.newClient(ydbConnection.getCtx().getGrpcTransport());
     }
 
+    @PostConstruct
     public void init() {
         for (int i = 0; i < ATTEMPT_CREATE_NODE; i++) {
             var status = coordinationClient.createNode(YDB_LOCK_NODE_NAME).join();
 
             if (status.isSuccess()) {
-                return;
+                coordinationSession = coordinationClient.createSession(YDB_LOCK_NODE_NAME);
+
+                var statusCS = coordinationSession.connect().join();
+
+                if (statusCS.isSuccess()) {
+                    logger.info("Created coordination node session [{}]", coordinationSession);
+
+                    return;
+                }
+                if (i == ATTEMPT_CREATE_NODE - 1) {
+                    statusCS.expectSuccess("Failed creating coordination node session");
+                }
             }
 
             if (i == ATTEMPT_CREATE_NODE - 1) {
@@ -44,22 +66,23 @@ public class YdbCoordinationServiceLockProvider implements LockProvider {
 
     @Override
     public Optional<SimpleLock> lock(LockConfiguration lockConfiguration) {
-        var coordinationSession = coordinationClient.createSession(YDB_LOCK_NODE_NAME);
+        logger.info("Instance[{}] is trying to become a leader...", INSTANCE_INFO);
 
-        coordinationSession.connect().join()
-                .expectSuccess("Failed creating coordination node session");
-
-        logger.debug("Created coordination node session");
-
-        var semaphoreLease = coordinationSession.acquireEphemeralSemaphore(lockConfiguration.getName(), true,
-                lockConfiguration.getLockAtMostFor()).join();
+        Result<SemaphoreLease> semaphoreLease = coordinationSession.acquireEphemeralSemaphore(
+                lockConfiguration.getName(),
+                true,
+                INSTANCE_INFO_BYTES,
+                lockConfiguration.getLockAtMostFor()
+        ).join();
 
         if (semaphoreLease.isSuccess()) {
-            logger.debug("Semaphore acquired");
+            logger.info("Instance[{}] acquired semaphore[SemaphoreName={}]", INSTANCE_INFO,
+                    semaphoreLease.getValue().getSemaphoreName());
 
             return Optional.of(new YdbSimpleLock(semaphoreLease.getValue()));
         } else {
-            logger.debug("Semaphore is not acquired");
+            logger.info("Instance[{}] did not acquire semaphore", INSTANCE_INFO);
+
             return Optional.empty();
         }
     }
@@ -67,12 +90,17 @@ public class YdbCoordinationServiceLockProvider implements LockProvider {
     private record YdbSimpleLock(SemaphoreLease semaphoreLease) implements SimpleLock {
         @Override
         public void unlock() {
+            logger.info("Instance[{}] released semaphore[SemaphoreName={}]", INSTANCE_INFO, semaphoreLease.getSemaphoreName());
+
             semaphoreLease.release().join();
         }
     }
 
     @PreDestroy
     private void close() throws SQLException {
+        // closing coordination session
+        coordinationSession.close();
+
         ydbConnection.close();
     }
 }
