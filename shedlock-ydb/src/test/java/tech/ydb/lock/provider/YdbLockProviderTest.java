3690721aec3e5f11cda9255ed2508f4c019b5dcc
Kirill Kurdyukov <kurdyukov-kir@ydb.tech>
2024-11-18 18:47:31 +0300

    feat: shedlock over jdbc (#168)

--- shedlock-ydb/src/test/java/tech/ydb/lock/provider/YdbLockProviderTest.java
+++ shedlock-ydb/src/test/java/tech/ydb/lock/provider/YdbLockProviderTest.java
@@ -1,17 +1,24 @@
 package tech.ydb.lock.provider;
 
+import java.sql.SQLException;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
+import javax.sql.DataSource;
 import net.javacrumbs.shedlock.core.LockConfiguration;
 import net.javacrumbs.shedlock.core.SimpleLock;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -23,6 +30,7 @@ import tech.ydb.test.junit5.YdbHelperExtension;
  */
 @SpringBootTest(classes = TestApp.class)
 public class YdbLockProviderTest {
+    private static final Logger logger = LoggerFactory.getLogger(YdbLockProviderTest.class);
 
     @RegisterExtension
     private static final YdbHelperExtension ydb = new YdbHelperExtension();
@@ -46,27 +54,44 @@ public class YdbLockProviderTest {
     }
 
     @Autowired
-    private YdbCoordinationServiceLockProvider lockProvider;
+    private YdbJDBCLockProvider lockProvider;
+
+    @Autowired
+    private DataSource dataSource;
 
     @Test
-    public void integrationTest() throws ExecutionException, InterruptedException {
+    public void integrationTest() throws ExecutionException, InterruptedException, SQLException {
+        try (var connection = dataSource.getConnection()) {
+            var statement = connection.createStatement();
+            statement.execute(
+                    "CREATE TABLE shedlock(" +
+                            "name TEXT NOT NULL, " +
+                            "lock_until TIMESTAMP NOT NULL," +
+                            "locked_at TIMESTAMP NOT NULL," +
+                            "locked_by TEXT NOT NULL, " +
+                            "PRIMARY KEY (name)" +
+                            ");");
+        }
+
+        var lockFutures = new ArrayList<Future<?>>();
         var executorServer = Executors.newFixedThreadPool(10);
         var atomicInt = new AtomicInteger();
         var locked = new AtomicBoolean();
 
-        for (int i = 0; i < 10; i++) {
-            executorServer.submit(() -> {
+        for (int i = 0; i < 100; i++) {
+            lockFutures.add(executorServer.submit(() -> {
                 Optional<SimpleLock> optinal = Optional.empty();
 
                 while (optinal.isEmpty()) {
-                    optinal = lockProvider.lock(
-                            new LockConfiguration(Instant.now(), "semaphore", Duration.ZERO, Duration.ZERO));
+                    optinal = lockProvider.lock(new LockConfiguration(
+                            Instant.now(), "semaphore", Duration.ofSeconds(100), Duration.ZERO));
 
                     optinal.ifPresent(simpleLock -> {
-                        if (locked.get()) {
+                        if (!locked.compareAndSet(false, true)) {
+                            logger.error("Failed test! System has two leaders");
+
                             throw new RuntimeException();
                         }
-                        locked.set(true);
 
                         try {
                             Thread.sleep(100);
@@ -76,12 +101,23 @@ public class YdbLockProviderTest {
 
                         atomicInt.addAndGet(50);
                         locked.set(false);
+                        logger.info("Leader does UNLOCK!");
                         simpleLock.unlock();
                     });
+
+                    try {
+                        Thread.sleep(ThreadLocalRandom.current().nextInt(3_000));
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
-            }).get();
+            }));
         }
 
-        Assertions.assertEquals(500, atomicInt.get());
+        for (var future : lockFutures) {
+            future.get();
+        }
+
+        Assertions.assertEquals(5000, atomicInt.get());
     }
 }
