36b7d74c74a8e4eba1d471423dd0a77b9fa9e41a
Alexandr Gorshenin <alexandr268@ydb.tech>
2025-05-16 10:33:12 +0100

    ydb-platformGH-180 Introduced YdbType complement type-safe variant

--- spring-data-jdbc-ydb/src/main/java/tech/ydb/data/core/convert/YdbType.java
+++ spring-data-jdbc-ydb/src/main/java/tech/ydb/data/core/convert/YdbType.java
@@ -1,15 +1,26 @@
 package tech.ydb.data.core.convert;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * The annotation for qualification of the target YDB data type.
+ *
  * @author Madiyar Nurgazin
+ * @author Mikhail Polivakha
+ * @deprecated Please, use {@link tech.ydb.data.core.convert.annotation.YdbType} instead because of type safety considerations.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
+@Deprecated(forRemoval = true)
+@Documented
 public @interface YdbType {
+    /**
+     * The target YDB data type.
+     * @return name of YDB data type
+     */
     String value();
 }
