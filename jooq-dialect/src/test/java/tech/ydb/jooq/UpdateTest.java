57e7d687e81288d4fb2c296383c0a66ab8b89cfd
Aleksandr Duplishchev <ppalex@yandex-team.ru>
2026-01-28 16:10:47 +0300

    tests

--- jooq-dialect/src/test/java/tech/ydb/jooq/UpdateTest.java
+++ jooq-dialect/src/test/java/tech/ydb/jooq/UpdateTest.java
@@ -38,6 +38,42 @@ public class UpdateTest extends BaseTest {
     }
 
     @Test
+    public void testSimpleUpdateWithoutAliasing() {
+        dsl.update(SERIES)
+                .set(SERIES.TITLE, "Updated Series One")
+                .set(SERIES.SERIES_INFO, "Updated Info One")
+                .where(SERIES.SERIES_ID.eq(ULong.valueOf(1)))
+                .execute();
+
+        SeriesRecord updatedRecord = dsl.selectFrom(SERIES)
+                .where(SERIES.SERIES_ID.eq(ULong.valueOf(1)))
+                .fetchOne();
+
+        assertNotNull(updatedRecord);
+        assertEquals("Updated Series One", updatedRecord.getTitle());
+        assertEquals("Updated Info One", updatedRecord.getSeriesInfo());
+    }
+
+    @Test
+    public void testUpdateSetToField() {
+        dsl.update(SERIES)
+                .set(SERIES.TITLE, SERIES.TITLE.concat(" updated"))
+                .set(SERIES.SERIES_INFO, SERIES.TITLE)
+                .set(SERIES.RELEASE_DATE, SERIES.RELEASE_DATE.plus(5))
+                .where(SERIES.SERIES_ID.eq(ULong.valueOf(1)))
+                .execute();
+
+        SeriesRecord updatedRecord = dsl.selectFrom(SERIES)
+                .where(SERIES.SERIES_ID.eq(ULong.valueOf(1)))
+                .fetchOne();
+
+        assertNotNull(updatedRecord);
+        assertEquals("Series One updated", updatedRecord.getTitle());
+        assertEquals("Series One", updatedRecord.getSeriesInfo());
+        assertEquals(ULong.valueOf(20220106), updatedRecord.getReleaseDate());
+    }
+
+    @Test
     public void testConditionalUpdate() {
         dsl.update(SERIES)
                 .set(SERIES.TITLE.as(Field::getName), "Updated Early Release")
