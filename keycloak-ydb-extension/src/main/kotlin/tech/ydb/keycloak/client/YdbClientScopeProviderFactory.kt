36d3c0ab273ccd12408e0b62645d616e18a6a148
Ainur Mukhtarov <aikamyx2004@gmail.com>
2026-02-15 15:34:09 +0300

    feat: Create ClientScopeProviderFactory implementation to return YdbRealmProvider instead of JpaRealmProvider

---
+++ keycloak-ydb-extension/src/main/kotlin/tech/ydb/keycloak/client/YdbClientScopeProviderFactory.kt
@@ -0,0 +1,19 @@
+package tech.ydb.keycloak.client
+
+import org.keycloak.connections.jpa.JpaConnectionProvider
+import org.keycloak.models.ClientScopeProvider
+import org.keycloak.models.KeycloakSession
+import org.keycloak.models.jpa.JpaClientScopeProviderFactory
+import tech.ydb.keycloak.config.ProviderPriority.PROVIDER_PRIORITY
+import tech.ydb.keycloak.realm.YdbRealmProvider
+
+class YdbClientScopeProviderFactory: JpaClientScopeProviderFactory() {
+  override fun create(session: KeycloakSession): ClientScopeProvider {
+    val em = session.getProvider(JpaConnectionProvider::class.java).entityManager
+    return YdbRealmProvider(session, em, null, null)
+  }
+
+  override fun order(): Int {
+    return PROVIDER_PRIORITY
+  }
+}
