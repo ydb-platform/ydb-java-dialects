a6476c14c18a531e5df72f22fde4d81194add12f
Ainur Mukhtarov <aikamyx2004@gmail.com>
2026-02-26 00:08:45 +0300

    refactor: Make PROVIDER_PRIORITY bigger to show that only YDB factories are used

--- keycloak-ydb-extension/src/main/kotlin/tech/ydb/keycloak/config/ProviderPriority.kt
+++ keycloak-ydb-extension/src/main/kotlin/tech/ydb/keycloak/config/ProviderPriority.kt
@@ -1,6 +1,5 @@
 package tech.ydb.keycloak.config
 
 object ProviderPriority {
-  // more than in quarkus provider factories
-  const val PROVIDER_PRIORITY = 200
+  const val PROVIDER_PRIORITY = Int.MAX_VALUE
 }
