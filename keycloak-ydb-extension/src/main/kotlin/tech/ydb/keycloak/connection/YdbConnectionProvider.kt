2cf789b3292d9e2ca689c179841b83ba1e4b5de0
Ainur Mukhtarov <aikamyx2004@gmail.com>
2026-02-26 00:03:41 +0300

    refactor: Deleted redundant files

--- keycloak-ydb-extension/src/main/kotlin/tech/ydb/keycloak/connection/YdbConnectionProvider.kt
+++
@@ -1,5 +0,0 @@
-package tech.ydb.keycloak.connection
-
-import org.keycloak.provider.Provider
-
-interface YdbConnectionProvider : Provider
--- keycloak-ydb-extension/src/main/kotlin/tech/ydb/keycloak/connection/YdbConnectionProviderFactory.kt
+++
@@ -1,5 +0,0 @@
-package tech.ydb.keycloak.connection
-
-import org.keycloak.provider.ProviderFactory
-
-interface YdbConnectionProviderFactory<T : YdbConnectionProvider> : ProviderFactory<T>
