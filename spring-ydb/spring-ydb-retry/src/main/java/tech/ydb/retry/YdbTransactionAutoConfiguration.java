cdf8cd0e3513677d307aa0dcd5c07110d0b1f54a
karambo3a <ikaterina0909@gmail.com>
2026-05-12 20:41:23 +0300

    change retry auto-configuration

--- spring-ydb/spring-ydb-retry/src/main/java/tech/ydb/retry/YdbTransactionAutoConfiguration.java
+++ spring-ydb/spring-ydb-retry/src/main/java/tech/ydb/retry/YdbTransactionAutoConfiguration.java
@@ -2,17 +2,14 @@ package tech.ydb.retry;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.interceptor.TransactionInterceptor;
 
-@Configuration
-@AutoConfigureBefore(TransactionAutoConfiguration.class)
+@AutoConfiguration
 @ConditionalOnClass(TransactionInterceptor.class)
 @EnableConfigurationProperties(YdbRetryProperties.class)
 public class YdbTransactionAutoConfiguration {
