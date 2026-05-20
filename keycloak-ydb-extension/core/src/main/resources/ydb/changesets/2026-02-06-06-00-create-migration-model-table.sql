f10ac44ced5a3ed1f6f08029407eff81b7b4c1c1
Ainur Mukhtarov <aikamyx2004@gmail.com>
2026-05-20 14:41:41 +0300

    chore: Add indexes as it is in PG. Remove redundant indexes. Add missing tables

--- keycloak-ydb-extension/core/src/main/resources/ydb/changesets/2026-02-06-06-00-create-migration-model-table.sql
+++ keycloak-ydb-extension/core/src/main/resources/ydb/changesets/2026-02-06-06-00-create-migration-model-table.sql
@@ -4,5 +4,8 @@ CREATE TABLE IF NOT EXISTS MIGRATION_MODEL
     `VERSION`    Utf8,
     `UPDATE_TIME` Int64 NOT NULL DEFAULT 0,
 
+    INDEX uk_migration_version GLOBAL UNIQUE ON (`VERSION`),
+    INDEX uk_migration_update_time GLOBAL UNIQUE ON (`UPDATE_TIME`),
+    INDEX idx_update_time GLOBAL ON (`UPDATE_TIME`),
     PRIMARY KEY (`ID`)
 );
--- keycloak-ydb-extension/core/src/main/resources/ydb/changesets/2026-02-06-21-24-create-client-attributes-table.sql
+++ keycloak-ydb-extension/core/src/main/resources/ydb/changesets/2026-02-06-21-24-create-client-attributes-table.sql
@@ -4,6 +4,7 @@ CREATE TABLE IF NOT EXISTS CLIENT_ATTRIBUTES
     `NAME`      Utf8 NOT NULL,
     `VALUE`     Utf8,
 
+    INDEX idx_client_att_by_name_value GLOBAL ON (`NAME`, `VALUE`),
 --     INDEX idx_client_att_by_name_value GLOBAL ON (`NAME`, SUBSTRING(`VALUE`, 1, 255)),
 --     not implemented in ydb...
     PRIMARY KEY (CLIENT_ID, NAME)
